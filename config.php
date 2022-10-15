622c81c7126efe9baae80b310ee60477d2394867
Dmitry Kanaev <dimmkan@mail.ru>
2022-10-15 11:51:01 +0300

    Fix

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ define ('SITE_PATH', $sitePath); // путь к корневой папке са
 
 // для подключения к бд
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'password');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'quest');
