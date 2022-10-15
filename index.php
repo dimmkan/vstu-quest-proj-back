d5e0d223e0eb8097c2011d99542e3e365c99e71b
Dmitry Kanaev <dimmkan@mail.ru>
2022-10-15 12:30:03 +0300

    fix

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
 // включим отображение всех ошибок
 error_reporting (E_ALL); 
+
+phpinfo();
 // подключаем конфиг
-include('config.php');
+// include('config.php');
 
-// Соединяемся с БД
-$dbObject = new PDO('mysql:host=' . DB_HOST . ';port=3307;dbname=' . DB_NAME, DB_USER, DB_PASS);
+// // Соединяемся с БД
+// $dbObject = new PDO('mysql:host=' . DB_HOST . ';port=3307;dbname=' . DB_NAME, DB_USER, DB_PASS);
 
-// подключаем ядро сайта
-include(SITE_PATH . DS . 'core' . DS . 'core.php');
+// // подключаем ядро сайта
+// include(SITE_PATH . DS . 'core' . DS . 'core.php');
 
-// Загружаем router
-$router = new Router($registry);
-// записываем данные в реестр
-$registry->set('router', $router);
-// задаем путь до папки контроллеров.
-$router->setPath(SITE_PATH . 'controllers');
-// запускаем маршрутизатор
-$router->start();
+// // Загружаем router
+// $router = new Router($registry);
+// // записываем данные в реестр
+// $registry->set('router', $router);
+// // задаем путь до папки контроллеров.
+// $router->setPath(SITE_PATH . 'controllers');
+// // запускаем маршрутизатор
+// $router->start();
